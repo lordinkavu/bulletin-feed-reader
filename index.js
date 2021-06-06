2a0a46ddc34f3e78885a387641993d0e58fa25c4
Gautham <gauthamvaranacode@gmail.com>
2021-06-06 23:46:50 +0530

    ready for deployment

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ const app = express();
 const dotenv = require("dotenv");
 dotenv.config();
 const db = require("./db/db-methods");
-const cors = require('cors');
-const path = require('path');
+const cors = require("cors");
+const path = require("path");
 
-app.use(cors({origin:true,credentials:true}));
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(cors({ origin: true, credentials: true }));
+
 const uri = `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@bullet.xvncb.mongodb.net/bulletdb?retryWrites=true&w=majority`;
 const db_obj = new db(uri);
 
@@ -19,20 +19,26 @@ db_obj
       console.log(`app listening on port ${port}`);
     });
   })
-  .catch(e => {
+  .catch((e) => {
     console.log(e);
   });
 
-app.get("/articles/:domain", async(req, res) => {
-  
-  const cursor = db_obj.client.db('bulletdb').collection('articles').find({domain:req.params.domain}).sort({pubDate:-1}).limit(50);
+app.get("/articles/:domain", async (req, res) => {
+  const cursor = db_obj.client
+    .db("bulletdb")
+    .collection("articles")
+    .find({ domain: req.params.domain })
+    .sort({ pubDate: -1 })
+    .limit(50);
   const articles = await cursor.toArray();
-  res.send({articles:articles});
+  res.send({ articles: articles });
 });
 
-
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-});
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "client/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client/build", "index.html"));
+  });
+}
 
 const port = process.env.PORT || 8080;
